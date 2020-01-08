144075c0dc210f978f666b7f306ee14ba9022d30
Pawel Jozwik X <pawel.jozwik2@sabre.com>
2020-01-08 21:30:35 +0100

    tech: Added product categories

--- lib/baby_linen/baby_linen_statistics_page.dart
+++ lib/baby_linen/baby_linen_statistics_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pregnancy_card/baby_linen/product_category.dart';
 import 'package:pregnancy_card/baby_linen/statistic_card.dart';
 
 class BabyLinenStatisticsPage extends StatefulWidget {
@@ -7,6 +8,19 @@ class BabyLinenStatisticsPage extends StatefulWidget {
 }
 
 class BabyLinenStatisticsPageState extends State<BabyLinenStatisticsPage> {
+  List<ProductCategory> productsCategories = [
+    new ProductCategory(
+        name: 'Clothes', completePercentage: 10, icon: Icons.spa),
+    new ProductCategory(
+        name: 'Bath & care', completePercentage: 20, icon: Icons.child_care),
+    new ProductCategory(
+        name: 'Feed', completePercentage: 30, icon: Icons.fastfood),
+    new ProductCategory(
+        name: 'Sleep', completePercentage: 40, icon: Icons.hotel),
+    new ProductCategory(
+        name: 'Others', completePercentage: 60, icon: Icons.child_friendly),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,33 +30,17 @@ class BabyLinenStatisticsPageState extends State<BabyLinenStatisticsPage> {
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: <Widget>[
-          StatisticCard(
-            title: 'Clothes',
-            completedPercentage: 10,
-            icon: Icons.spa,
-          ),
-          StatisticCard(
-              title: 'Bath & care',
-              completedPercentage: 20,
-              icon: Icons.child_care),
-          StatisticCard(
-              title: 'Feed',
-              completedPercentage: 30,
-              icon: Icons.fastfood
-          ),
-          StatisticCard(
-              title: 'Sleep',
-              completedPercentage: 40,
-              icon: Icons.hotel
-          ),
-          StatisticCard(
-              title: 'Others',
-              completedPercentage: 60,
-              icon: Icons.child_friendly),
-        ],
+      body: GridView.builder(
+        itemCount: productsCategories.length,
+        gridDelegate:
+            SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return StatisticCard(
+            title: productsCategories[index].name,
+            completedPercentage: productsCategories[index].completePercentage,
+            icon: productsCategories[index].icon,
+          );
+        },
       ),
     );
   }
---
+++ lib/baby_linen/product_category.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class ProductCategory {
+  final int id;
+  final String name;
+  final double completePercentage;
+  final IconData icon;
+
+  ProductCategory({this.id, this.name, this.completePercentage, this.icon});
+
+}
